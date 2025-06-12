81d8d4f337afb5f7ab6ae1c2c78b3ad10d50f161
aghabidareh <aghabidareh@gmail.com>
2025-06-12 12:24:32 +0330

    loading-models

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ import open3d as o3d
 from PIL import Image
 from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 
-
+model_type = "DPT_Large"
+midas = torch.hub.load("intel-isl/MiDaS", model_type, pretrained=True)
+midas.eval()
